1340384e27c83553fa8b844d10c57d0deacf4ca3
spartan007-28 <52513522+spartan007-28@users.noreply.github.com>
2020-02-14 11:48:26 -0600

    Update db_conexion.js

--- server/config/db_conexion.js
+++ server/config/db_conexion.js
@@ -4,8 +4,8 @@ const { Pool } = require('pg')
 const pool = new Pool({
     user: "postgres",
     host: "ti.aesantarita.com.mx",
-    database: "aes_transporte",
-    password: "xRw85w$92E",
+    database: "*********",
+    password: "********",
     port: 5432
 });
 
